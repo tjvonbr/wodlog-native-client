5f940499c23f500c2e157431404190357948ecb3
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2020-11-21 00:00:17 -0600

    trying to fix edit/delete modal

--- components/ExerciseListItem.js
+++ components/ExerciseListItem.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import DeleteExerciseModal from '../components/DeleteExerciseModal';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const ExerciseListItem = ({ item }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [exercise, setExercise] = useState(item);
+
+  useEffect(() => {
+    console.log(item)
+  }, [item])
+
+  const showModal = () => setIsVisible(true);
+  const hideModal = () => setIsVisible(false);
+
+  function deleteExercise() {
+    axios.delete(`${exercisesApi}/:${id}`)
+      .then(res => {
+        setExercises(res.data);
+        hideModal();
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
-    <View style={styles.wrapper}>
-      <Text style={styles.itemText}>{item.name}</Text>
+    <View>
+    <TouchableOpacity
+      activeOpacity={0.1}
+      onLongPress={showModal}
+    >
+      <View style={styles.wrapper}>
+        <Text style={styles.itemText}>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
+    <View>
+        <DeleteExerciseModal 
+          visible={isVisible}
+          handleClose={hideModal}
+          handleChange={handleChange}
+          reset={resetDelInputs}
+        />
+      </View>
     </View>
   )
 }
